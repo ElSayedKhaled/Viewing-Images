fea1e61a0009094a22587ac2a624448bd15836f1
El Sayed Khaled <elsayedelnagar11@gmail.com>
2021-08-26 23:31:04 +0200

    Add files via upload

---
+++ e-File_Task/e-File_Task/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace e_File_Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.IconList = new System.Windows.Forms.ImageList(this.components);
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.AddressBox = new System.Windows.Forms.TextBox();
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.IconList;
+            this.listView1.Location = new System.Drawing.Point(5, 43);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(270, 596);
+            this.listView1.SmallImageList = this.IconList;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // IconList
+            // 
+            this.IconList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("IconList.ImageStream")));
+            this.IconList.TransparentColor = System.Drawing.Color.Transparent;
+            this.IconList.Images.SetKeyName(0, "folder.png");
+            this.IconList.Images.SetKeyName(1, "image.png");
+            this.IconList.Images.SetKeyName(2, "files.png");
+            this.IconList.Images.SetKeyName(3, "folder2.png");
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(66, 13);
+            this.filePathTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(132, 20);
+            this.filePathTextBox.TabIndex = 6;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(203, 13);
+            this.goButton.Margin = new System.Windows.Forms.Padding(2);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(58, 27);
+            this.goButton.TabIndex = 5;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(4, 9);
+            this.backButton.Margin = new System.Windows.Forms.Padding(2);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(58, 27);
+            this.backButton.TabIndex = 4;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(281, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(705, 596);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameBox.ForeColor = System.Drawing.Color.LightGray;
+            this.NameBox.Location = new System.Drawing.Point(24, 54);
+            this.NameBox.Multiline = true;
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(182, 33);
+            this.NameBox.TabIndex = 15;
+            this.NameBox.Enter += new System.EventHandler(this.NameBox_Enter);
+            this.NameBox.Leave += new System.EventHandler(this.NameBox_Leave);
+            // 
+            // AddressBox
+            // 
+            this.AddressBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddressBox.ForeColor = System.Drawing.Color.LightGray;
+            this.AddressBox.Location = new System.Drawing.Point(24, 108);
+            this.AddressBox.Multiline = true;
+            this.AddressBox.Name = "AddressBox";
+            this.AddressBox.Size = new System.Drawing.Size(182, 33);
+            this.AddressBox.TabIndex = 16;
+            this.AddressBox.Enter += new System.EventHandler(this.AddressBox_Enter);
+            this.AddressBox.Leave += new System.EventHandler(this.AddressBox_Leave);
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailBox.ForeColor = System.Drawing.Color.LightGray;
+            this.EmailBox.Location = new System.Drawing.Point(26, 157);
+            this.EmailBox.Multiline = true;
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(182, 33);
+            this.EmailBox.TabIndex = 17;
+            this.EmailBox.Enter += new System.EventHandler(this.EmailBox_Enter);
+            this.EmailBox.Leave += new System.EventHandler(this.EmailBox_Leave);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.NameBox);
+            this.panel1.Controls.Add(this.EmailBox);
+            this.panel1.Controls.Add(this.AddressBox);
+            this.panel1.Location = new System.Drawing.Point(992, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(291, 326);
+            this.panel1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(223, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Adress";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(227, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Email";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(92, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 35);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1281, 644);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.ImageList IconList;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox AddressBox;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ e-File_Task/e-File_Task/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace e_File_Task
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "E:";
+        private bool isFile = false;
+        private string currentlySelectedItemName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filePathTextBox.Text = filePath;
+            loadFilesAndDirectories();
+            NameBox.Text = "Name...";
+            AddressBox.Text = "Address...";
+            EmailBox.Text = "email@examle.com";
+        }
+        public void loadFilesAndDirectories()
+        {
+            DirectoryInfo fileList;
+            string tempFilePath = "";
+            FileAttributes fileAttr;
+            try
+            {
+
+                if (isFile)
+                {
+                    tempFilePath = filePath + "/" + currentlySelectedItemName;
+                    FileInfo fileDetails = new FileInfo(tempFilePath);
+                    fileAttr = File.GetAttributes(tempFilePath);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox1.Image = new Bitmap(tempFilePath);
+                    //Process.Start(tempFilePath);
+                }
+                else
+                {
+                    fileAttr = File.GetAttributes(filePath);
+
+                }
+
+                if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    fileList = new DirectoryInfo(filePath);
+                    FileInfo[] files = fileList.GetFiles(); // GET ALL THE FILES
+                    DirectoryInfo[] dirs = fileList.GetDirectories(); // GET ALL THE DIRS
+                    string fileExtension = "";
+                    listView1.Items.Clear();
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        fileExtension = files[i].Extension.ToUpper();
+                        switch (fileExtension)
+                        {
+                            case ".PNG":
+                            case ".JPG":
+                            case ".JPEG":
+                                listView1.Items.Add(files[i].Name, 1);
+                                break;
+
+                            default:
+                                //listView1.Items.Add(files[i].Name, 2);
+                                break;
+                        }
+                    }
+
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                        listView1.Items.Add(dirs[i].Name, 3);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        public void goBack()
+        {
+            try
+            {
+                removeBackSlash();
+                string path = filePathTextBox.Text;
+                path = path.Substring(0, path.LastIndexOf("/"));
+                this.isFile = false;
+                filePathTextBox.Text = path;
+                removeBackSlash();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        public void loadButtonAction()
+        {
+            removeBackSlash();
+            filePath = filePathTextBox.Text;
+            loadFilesAndDirectories();
+            isFile = false;
+        }
+
+        public void removeBackSlash()
+        {
+            string path = filePathTextBox.Text;
+            if (path.LastIndexOf("/") == path.Length - 1)
+            {
+                filePathTextBox.Text = path.Substring(0, path.Length - 1);
+            }
+        }
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            goBack();
+            loadButtonAction();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            currentlySelectedItemName = e.Item.Text;
+
+            FileAttributes fileAttr = File.GetAttributes(filePath + "/" + currentlySelectedItemName);
+            if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                isFile = false;
+                filePathTextBox.Text = filePath + "/" + currentlySelectedItemName;
+            }
+            else
+            {
+                isFile = true;
+            }
+
+        }
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameBox_Enter(object sender, EventArgs e)
+        {
+            if (NameBox.Text == "Name...")
+            {
+                NameBox.Text = "";
+                NameBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void NameBox_Leave(object sender, EventArgs e)
+        {
+            if (NameBox.Text.Trim() == "")
+            {
+                NameBox.Text = "Name...";
+                NameBox.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void AddressBox_Enter(object sender, EventArgs e)
+        {
+            if (AddressBox.Text == "Address...")
+            {
+                AddressBox.Text = "";
+                AddressBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void AddressBox_Leave(object sender, EventArgs e)
+        {
+            if (AddressBox.Text.Trim() == "")
+            {
+                AddressBox.Text = "Address...";
+                AddressBox.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void EmailBox_Enter(object sender, EventArgs e)
+        {
+            if (EmailBox.Text == "email@examle.com")
+            {
+                EmailBox.Text = "";
+                EmailBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void EmailBox_Leave(object sender, EventArgs e)
+        {
+            if (EmailBox.Text.Trim() == "")
+            {
+                EmailBox.Text = "email@examle.com";
+                EmailBox.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=SAYED;Initial Catalog=Viewing_Images;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Insert Into Image_Data (Name,Address,Email,Image)" +
+                "Values('" + NameBox.Text + "','" + AddressBox.Text + "','" + EmailBox.Text + "',@pic)", con);
+            MemoryStream stream = new MemoryStream();
+            pictureBox1.Image.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] pic = stream.ToArray();
+            cmd.Parameters.AddWithValue("@pic", pic);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Image Saved Successfully");
+        }
+    }
+}
